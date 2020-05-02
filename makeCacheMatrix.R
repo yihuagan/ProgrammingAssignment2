5ed367a7beb6db0661b734b619e8c75eedbe7428
yihuagan <62923962+yihuagan@users.noreply.github.com>
2020-05-02 00:38:53 -0700

    Add files via upload

---
+++ makeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix<-function(x=matrix()){
+inv<-NULLset<-function(y){
+  x<<-y
+  inv<<-NULL
+  }
+  get<-function()x
+  setInverse<-function(inverse)inv<<-inverse
+  getInverse<-function()inv
+  list(set=set,
+       get=get,
+       setInverse=setInverse,
+       getInverse=getInverse)
+}
